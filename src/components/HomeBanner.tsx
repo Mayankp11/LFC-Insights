d1816101004a1bd8de74eb093c2bcd017c9b5918
Mayank <mayankpatil2605@gmail.com>
2025-10-06 21:46:15 -0500

    style(HomeBanner): fixed the box size

--- src/components/HomeBanner.tsx
+++ src/components/HomeBanner.tsx
@@ -4,16 +4,17 @@ import { Box, Image } from "@chakra-ui/react";
 const HomeBanner: React.FC = () => {
   return (
     <Box
-      w="100%"
-      h={{ base: "250px", md: "400px", lg: "500px" }}
+      w="100vw" // takes up 90% of the viewport width
+      h={{ base: "40vh", md: "50vh", lg: "60vh" }} // responsive height based on viewport
+      mx="auto" // centers the banner horizontally
       overflow="hidden"
       borderRadius="xl"
       boxShadow="md"
     >
       <Image
-        src="/images/HomeBanner.png" 
+        src="/images/HomeBanner.png"
         alt="Home Banner"
-        objectFit="cover"
+        objectFit="contain" // ensures full image is visible (no cropping)
         w="100%"
         h="100%"
       />
